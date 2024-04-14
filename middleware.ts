0340e05105336982dde814d2c400366c99fd4582
Brayan <joan300501@gmail.com>
2024-04-14 18:12:13 -0500

    Refactor middleware.ts and tabs.tsx files

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,30 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
 
-import type { NextRequest } from 'next/server'
-
+import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
-  const { data: { session } } = await supabase.auth.getSession();
-  
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   if (!session) {
     // Redirect to login if there is no session
     if (!req.nextUrl.pathname.includes("/sign-in")) {
-      return NextResponse.redirect(new URL('/sign-in', req.url));
+      // return NextResponse.redirect(new URL('/sign-in', req.url));
     }
   } else if (req.nextUrl.pathname.includes("/sign-in")) {
-    return NextResponse.redirect(new URL('/dashboard', req.url));
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   } else {
     NextResponse.next();
   }
-  
-  return res
-}
 
+  return res;
+}
 
 // Ensure the middleware is only called for relevant paths.
 export const config = {
-  matcher: [
-
-   '/dashboard/:path*','/sign-in'
-  ],
-}
+  matcher: ["/dashboard/:path*", "/sign-in"],
+};
