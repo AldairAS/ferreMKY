277b6c828935fe452a890901e7ebedf2616ae189
Brayan <joan300501@gmail.com>
2024-04-15 10:57:36 -0500

    Refactor NavigationMenu component to add breadcrumb functionality

--- components/ui/shared/menu.tsx
+++ components/ui/shared/menu.tsx
@@ -47,6 +47,7 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import FerreMYKLogo from "/public/icon.png";
 import ProfileSheet from "@/components/profile-config-sheet";
+import { useEffect, useState } from "react";
 
 const menuItems = [
   {
@@ -76,10 +77,34 @@ const menuItems = [
   },
 ];
 
+type TBreadcrumb = {
+  label: string;
+  href: string;
+};
+
 export default function NavigationMenu() {
   const router = useRouter();
   const pathname = usePathname();
   const { setTheme } = useTheme();
+  const [breadcrumbs, setBreadcrumbs] = useState<TBreadcrumb[]>([]);
+  const [activePage, setActivePage] = useState("");
+
+  useEffect(() => {
+    if (router) {
+      const linkPath = pathname.split("/").filter((path) => path !== "");
+      linkPath.shift();
+
+      const pathArray = linkPath.map((path, index) => {
+        return {
+          label: path.charAt(0).toUpperCase() + path.slice(1),
+          href: `/${linkPath.slice(0, index + 1).join("/")}`,
+        };
+      });
+
+      setBreadcrumbs(pathArray as []);
+      setActivePage(linkPath[linkPath.length - 1]);
+    }
+  }, [router, pathname]);
 
   return (
     <div>
@@ -117,10 +142,10 @@ export default function NavigationMenu() {
         <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <div>
-                <SettingsIcon className="h-5 w-5" />
+              <Button className="px-2" variant="ghost">
+                <SettingsIcon className="h-5 w-5 text-muted-foreground" />
                 <span className="sr-only">Configuración</span>
-              </div>
+              </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="ml-5" align="center">
               <DropdownMenuLabel>Apariencia</DropdownMenuLabel>
@@ -141,7 +166,6 @@ export default function NavigationMenu() {
                 <ListCollapse className="h-4 w-4 mr-1" />
                 Collapsar
               </DropdownMenuItem>
-              <DropdownMenuSeparator />
             </DropdownMenuContent>
           </DropdownMenu>
         </nav>
@@ -204,20 +228,24 @@ export default function NavigationMenu() {
           </Sheet>
           <Breadcrumb className="hidden md:flex">
             <BreadcrumbList>
-              <BreadcrumbItem>
-                <BreadcrumbLink asChild>
-                  <Link href="home">Panel de Control</Link>
-                </BreadcrumbLink>
-              </BreadcrumbItem>
+              {breadcrumbs.map((breadcrumb, i) => (
+                <BreadcrumbItem key={i}>
+                  <BreadcrumbLink asChild>
+                    <Link
+                      href={breadcrumb.href}
+                      onClick={() => setActivePage(breadcrumb.label)}
+                    >
+                      {breadcrumb.href === "/dashboard"
+                        ? "Panel de Control"
+                        : breadcrumb.label}
+                    </Link>
+                  </BreadcrumbLink>
+                  {i < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
+                </BreadcrumbItem>
+              ))}
               <BreadcrumbSeparator />
               <BreadcrumbItem>
-                <BreadcrumbLink asChild>
-                  <Link href="/">Pedidos</Link>
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Pedidos Recientes</BreadcrumbPage>
+                <BreadcrumbPage>{activePage}</BreadcrumbPage>
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
@@ -252,9 +280,7 @@ export default function NavigationMenu() {
             <DropdownMenuContent align="end">
               <DropdownMenuLabel>Mi Cuenta</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <ProfileSheet />
-              </DropdownMenuItem>
+              <DropdownMenuItem>Perfil</DropdownMenuItem>
               <DropdownMenuItem>Soporte</DropdownMenuItem>
               <DropdownMenuSeparator />
               <DropdownMenuItem
